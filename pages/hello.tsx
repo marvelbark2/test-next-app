31f4beb3b53386c20ef17b79a236c8313cce6c09
Masaoudi youness <53633390+marvelbark2@users.noreply.github.com>
2023-01-16 06:40:27 +0100

    Add isMounted

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
+
+function useMounted() {
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => setMounted(true), [])
+
+    return mounted
+}
 function Hello() {
-    return ( 
+
+    const mounted = useMounted();
+
+    if (!mounted) return <span>Loading...</span>;
+    return (
         <h1>Hello world</h1>
-     );
+    );
 }
 
 export default Hello;
